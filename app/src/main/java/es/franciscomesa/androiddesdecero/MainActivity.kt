df582c7c11d6deca7a2e4e612b6ffe3dc99a5336
Francisco Mesa <franciscomesa@gmail.com>
2022-03-01 22:35:39 +0000

    ✨ get access to view component, classic style

--- app/src/main/java/es/franciscomesa/androiddesdecero/MainActivity.kt
+++ app/src/main/java/es/franciscomesa/androiddesdecero/MainActivity.kt
@@ -2,10 +2,25 @@ package es.franciscomesa.androiddesdecero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import es.franciscomesa.androiddesdecero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Classic way
+        val message = findViewById<TextView>(R.id.message)
+        message.text = "Hola mundo"
+
+        Log.d("MainActivity", "onCreate")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.d("MainActivity", "onDestroy")
     }
 }
